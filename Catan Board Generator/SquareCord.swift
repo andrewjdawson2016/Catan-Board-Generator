b70b2d9d5f1cd6985baab8d92df7323a7be4bb5b
Derek Dawson <derekhdawson@yahoo.com>
2016-11-25 11:14:38 -0800

    first draft UI

---
+++ Catan Board Generator/SquareCord.swift
@@ -0,0 +1,13 @@
+//
+//  SquareCord.swift
+//  Catan Board Generator
+//
+//  Created by Derek Dawson on 11/24/16.
+//  Copyright © 2016 Derek Dawson. All rights reserved.
+//
+
+import UIKit
+
+class SquareCord: NSObject {
+
+}
---
+++ Catan Board Generator/UITile.swift
@@ -0,0 +1,21 @@
+//
+//  UITile.swift
+//  Catan Board Generator
+//
+//  Created by Derek Dawson on 11/24/16.
+//  Copyright © 2016 Derek Dawson. All rights reserved.
+//
+
+import UIKit
+
+class UITile: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
